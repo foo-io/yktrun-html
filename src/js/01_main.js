7568b5ebccd76aaf685a34a23b59dd94e33c66e6
foo-io <mich@jzxworld.ru>
2022-01-05 23:43:42 +0900

    license

---
+++ src/js/01_main.js
@@ -0,0 +1,6 @@
+var myFullpage = new fullpage('#fullpage', {
+	verticalCentered: false,
+	navigation: true,
+	navigationPosition: 'right',
+	navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+});
